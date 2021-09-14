8e869942cf81a09b2070045bcd9540ba8e605e31
86152 <1096315853@qq.com>
2021-09-14 10:09:09 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/home/home.js
+import request from '../../service/network'
 Page({
 
   /**
@@ -12,55 +13,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 发送网络请求
+    // wx.request({
+    //   url: 'http://152.136.185.210:7878/api/m5/recommend',
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
 
+    // 使用封装的request发送网络请求
+    request({
+      url: 'http://152.136.185.210:7878/api/m5/recommend'
+    }).then(res =>{
+      console.log(res)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
